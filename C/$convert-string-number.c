0bcbe95c8d6d2f5566a91089d149f3f5b4af95e9
lijealso <lijealso@gmail.com>
2022-07-26 22:33:32 +0100

    convert string number done

---
+++ C/$convert-string-number.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char word[] = "-119";
+
+int string_to_number(const char *src) {
+
+    int x = atoi(src);
+
+
+    return 0;
+}
+
+int main(){
+    string_to_number(word);
+}
