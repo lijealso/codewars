8577cfe6a4a42082254671bc0c7a46a018f02343
lijealso <lijealso@gmail.com>
2022-07-29 00:43:04 +0100

    sum arrays done

---
+++ C/$sum-arrays.c
@@ -0,0 +1,11 @@
+#include <stddef.h>
+
+int sum_array(const int *values, size_t count){
+  
+  int sum = 0;
+  for (size_t i  = 0 ; i < count ; i++)
+  {
+    sum += values[i];  
+  }
+  return sum;
+}
