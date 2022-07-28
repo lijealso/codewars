b5dd1d0d76e6bddd2b5f001955a2a92a3963a9dd
lijealso <lijealso@gmail.com>
2022-07-28 20:15:43 +0100

    divisible by x and y done

---
+++ C/$divisible-by-x-and-y.c
@@ -0,0 +1,14 @@
+#include <stdbool.h>
+
+bool isDivisible(int n, int x, int y) {
+
+    if (n % x == 0 && n % y == 0)
+      {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+
+}
